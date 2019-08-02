05326b51eb2227b8100acd8bca47bc414a12676a
Ивлев Вадим <ivlev@rg.ru>
2019-08-02 17:51:01 +0300

    .

--- push.sh
+++ push.sh
@@ -2,6 +2,12 @@
 git add -A .
 git commit -m "."
 
+git push gitlab primitive
+git push github primitive
+git push origin primitive
+
 git push gitlab master
 git push github master
 git push origin master
+
+
