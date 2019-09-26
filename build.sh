0142074ff837b0f90a228bdbdcf8d146b40068a3
Ивлев Вадим <ivlev@rg.ru>
2019-09-26 16:58:36 +0300

    fix: comments

--- build.sh
+++ build.sh
@@ -3,11 +3,12 @@
 echo "building ..."
 export GO111MODULE=on
 
-# линкуем статически под линукс
 # env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a  .
 
+# линкуем статически под линукс
 # env CGO_ENABLED=0 GOOS=linux go build -a -ldflags '-extldflags "-static"' . 
 
+# CGO_ENABLED=1 нужно для драйвера SQLite
 env CGO_ENABLED=1 GOOS=linux go build -a -ldflags '-linkmode external -extldflags "-static"' .  || exit 1
 
 
