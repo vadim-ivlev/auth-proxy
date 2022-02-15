510dc0d67fd9e7f2936cfdb0059ed353fb2a2265
vadim-ivlev <vadim.ivlev@gmail.com>
2022-02-16 02:05:12 +0300

    add: group views.

--- migrations/02_create_views.down.sql
+++ migrations/02_create_views.down.sql
@@ -1,2 +1,5 @@
 DROP VIEW IF EXISTS app_user_role_extended;
+DROP VIEW IF EXISTS group_user_role_extended;
+DROP VIEW IF EXISTS group_app_role_extended;
+
 
--- migrations/02_create_views.up.sql
+++ migrations/02_create_views.up.sql
@@ -22,3 +22,40 @@ CREATE VIEW app_user_role_extended AS
     INNER JOIN app       AS a   ON aur.appname  = a.appname
 ;
 
+DROP VIEW IF EXISTS group_user_role_extended;
+CREATE VIEW group_user_role_extended AS
+    SELECT 
+        gu.group_id   AS group_id,
+        gu.user_id    AS user_id,
+        gu.rolename   AS rolename,
+
+        g.groupname   AS group_groupname,
+        g.description AS group_description,
+
+        u.email       AS user_email,
+        u.fullname    AS user_fullname,
+        u.description AS user_description,
+        u.disabled    AS user_disabled
+        
+    FROM "group" g
+    JOIN group_user_role gu ON gu.group_id = g.id
+    JOIN "user" u           ON u.id = gu.user_id
+;
+
+DROP VIEW IF EXISTS group_app_role_extended;
+CREATE VIEW group_app_role_extended AS
+    SELECT 
+        ga.group_id   AS group_id,
+        ga.app_id     AS app_id,
+        ga.rolename   AS rolename,
+
+        g.groupname   AS group_groupname,
+        g.description AS group_description,
+
+        a.description  AS app_description,
+        a.url          AS app_url
+           
+    FROM "group" g
+    JOIN group_app_role ga ON ga.group_id = g.id
+    JOIN app a             ON a.id = ga.app_id
+;
