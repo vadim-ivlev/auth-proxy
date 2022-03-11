778b0d74b8c8f29e00de453b29a34ce5f3b8e97d
vadim-ivlev <vadim.ivlev@gmail.com>
2022-03-11 05:15:55 +0300

    fix up.sh.

--- sh/up.sh
+++ sh/up.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+echo 'порождаем auth_proxy_network'
+docker network create auth_proxy_network
+
 echo 'поднимаем бд'
 docker-compose -f docker-compose-dev.yml up -d
 
 # echo 'спим 2 секунды'
 # sleep 2
 
+
 echo 'запускаем приложение'
 go run main.go -port 4400 -config=./configs/app.env.dev -pgconfig=./configs/db.env.dev
+
 
