7ac189b5885814aabe2e45bbb0586ecee599df77
vadim-ivlev <vadim.ivlev@gmail.com>
2022-03-30 20:11:02 +0300

    fix: compiling front container with CGO_ENABLED=0.

--- sh/build-frontend-container.sh
+++ sh/build-frontend-container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Кросскомпиляция на компьютере разработчика auth-proxy:front"
-env GOOS=linux GOARCH=amd64 go build -tags=jsoniter .
+env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -tags=jsoniter .
 
 
 echo "build a docker image auth-proxy:front"
