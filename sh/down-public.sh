d5b7034018dee2b93ec7c68e0e28f5dda8918472
vadim-ivlev <vadim.ivlev@gmail.com>
2022-04-11 04:34:45 +0300

    fix: cleaning. removing public.

--- sh/down-public.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo 'гасим докер'
-docker-compose -f docker-compose-public.yml down -v --remove-orphans 
--- sh/up-public.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo 'поднимаем докер'
-docker-compose -f docker-compose-public.yml up -d && docker logs -f auth-proxy-public 
