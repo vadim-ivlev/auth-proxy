d29fe102bed5f50a604286bc76c38b07fc770273
Максим Чагин <maxchagin@gmail.com>
2022-06-02 16:53:04 +0300

    update: sh deploy on prod

--- sh/deploy-prod-rg.sh
+++ sh/deploy-prod-rg.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "deploying on production rg ..."
-git push origin master:production
+date=$(date '+%d%m%Y-%H%M')
+git tag prod_$date
+git push origin prod_$date
 echo "Watch pipeline -> https://git.rgwork.ru/masterback/auth-proxy/pipelines"
-
