1adec24523c58e6bb78cc848fa9939c87bd24df9
vadim-ivlev <vadim.ivlev@gmail.com>
2023-04-28 14:31:36 +0100

    fix: WIP.

--- server/log_requests.go
+++ server/log_requests.go
@@ -13,24 +13,29 @@ import (
 // logCreateUser - логирование запроса на создание пользователя
 func logCreateUser(params graphql.ResolveParams) {
 	ArgToLowerCase(params, "username")
-	c, _ := params.Context.Value("ginContext").(*gin.Context)
+	c, ok := params.Context.Value("ginContext").(*gin.Context)
+	if !ok {
+		log.Println("logCreateUser error: ginContext not found")
+		return
+	}
+
 	email, _ := params.Args["email"].(string)
 	password, _ := params.Args["password"].(string)
 	fullname, _ := params.Args["fullname"].(string)
 	description, _ := params.Args["description"].(string)
-
 	fullPath := c.FullPath()
 	headers := app.Serialize(c.Request.Header)
 	ip := c.ClientIP()
 	userAgent := c.Request.UserAgent()
 	referer := c.Request.Referer()
 
-	log.Printf("email=%s, password=%s fullname=%s description=%s \n", email, password, fullname, description)
-	log.Printf("ip=%s, user_agent=%s\n", ip, userAgent)
-	log.Printf(" FullPath: %v\n", fullPath)
-	log.Printf(" Headers: %v\n", headers)
-	log.Printf(" Referer: %v\n", referer)
+	// log.Printf("email=%s, password=%s fullname=%s description=%s \n", email, password, fullname, description)
+	// log.Printf("ip=%s, user_agent=%s\n", ip, userAgent)
+	// log.Printf(" FullPath: %v\n", fullPath)
+	// log.Printf(" Headers: %v\n", headers)
+	// log.Printf(" Referer: %v\n", referer)
 
+	// запись в лог
 	_, err := db.QueryExec("INSERT INTO create_user_log (email, password, fullname, description, ip, user_agent, full_path, referer, headers) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)",
 		email, password, fullname, description, ip, userAgent, fullPath, referer, headers)
 	if err != nil {
